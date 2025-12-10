187b0c0d942df59be730fb67dbc40d110f649db0
userdefault13 <user.default@me.com>
2025-12-09 22:24:53 -0800

    Move CSS import to app.vue to ensure Tailwind utilities are included

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  // CSS imported in app.vue to ensure Tailwind utilities are included globally
   postcss: {
     plugins: {
       tailwindcss: {},
